987828f934249a13be6c6d8fb42721fc94c45cd7
Andrei Marian Patru <andreiuttzzumarian00@gmail.com>
2021-09-22 12:16:18 +0300

    text input no functionality

--- src/features/Welcome/welcome.js
+++ src/features/Welcome/welcome.js
@@ -1,15 +1,35 @@
 import React, { Fragment } from 'react'
 import { Typography, Grid } from '@material-ui/core'
-import { useToast } from '@bit/totalsoft_oss.react-mui.kit.core'
+import IconButton from '@bit/totalsoft_oss.react-mui.icon-button'
+import { CustomTextField } from '@bit/totalsoft_oss.react-mui.kit.core'
+import KeyboardReturnIcon from '@material-ui/icons/KeyboardReturn'
+
+import { useTranslation } from 'react-i18next'
 
 function Welcome() {
-  const addToast = useToast()
-  addToast('This is my toast', 'success')
+  const { t } = useTranslation()
+
   return (
-    <Fragment>
-      <Typography>This is my dashboard...</Typography>
-      <Grid>This can be seen by any logged in users.</Grid>
-    </Fragment>
+    <Grid container justify='center' alignItems='center' alignContent='center' direction='column' spacing={10}>
+      <Grid item xs={4}>
+        <Typography variant='h5'>{t('LandingPage.Title')}</Typography>
+      </Grid>
+      <Grid item container justify='center' alignItems='center' alignContent='center' direction='column' spacing={1}>
+        <Grid item xs={12}>
+          <Typography variant='caption'>{t('LandingPage.Subtitle')}</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <CustomTextField
+            fullWidth
+            endAdornment={
+              <IconButton size='small' color='theme' aria-label='go'>
+                <KeyboardReturnIcon fontSize='small' />
+              </IconButton>
+            }
+          />
+        </Grid>
+      </Grid>
+    </Grid>
   )
 }
 
