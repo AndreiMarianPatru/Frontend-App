2f73e17c44d935ea7f9966d4a4ae36a63d5f5ffe
Andrei Marian Patru <andreiuttzzumarian00@gmail.com>
2021-09-23 16:59:29 +0300

    buttons

--- src/features/conference/components/ConferenceItem.js
+++ src/features/conference/components/ConferenceItem.js
@@ -1,18 +1,26 @@
-import RegularCard from '@bit/totalsoft_oss.react-mui.regular-card'
 import React from 'react'
 import PropTypes from 'prop-types'
+import RegularCard from '@bit/totalsoft_oss.react-mui.regular-card'
+//import RegularCard from 'components/common/cards/RegularCard';
 import ConferenceSubtitle from './ConferenceSubtitle'
+import ConferenceContent from './ConferenceContent'
+
 const ConferenceItem = props => {
   const { conference } = props
-  const { name, location, speakers } = conference
+  const { name, speakers, location } = conference
   const speaker = speakers.find(speaker => speaker.isMainSpeaker)
 
   return (
-    <RegularCard cardTitle={name} cardSubtitle={<ConferenceSubtitle speaker={speaker} location={location} />} content={'content here'} />
+    <RegularCard
+      cardTitle={name}
+      cardSubtitle={<ConferenceSubtitle speaker={speaker} location={location} />}
+      content={<ConferenceContent conference={conference} />}
+    />
   )
 }
 
 ConferenceItem.propTypes = {
   conference: PropTypes.object.isRequired
 }
+
 export default ConferenceItem
