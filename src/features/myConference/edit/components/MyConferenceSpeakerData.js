87744b7081aa2c6e2900d5dd5a50ba4eff57df29
Andrei Marian Patru <andreiuttzzumarian00@gmail.com>
2021-09-24 17:19:52 +0300

    speakers table

---
+++ src/features/myConference/edit/components/MyConferenceSpeakerData.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Td, Tr } from 'react-super-responsive-table'
+import tableStyles from 'assets/jss/components/tableStyle'
+import { makeStyles, Checkbox } from '@material-ui/core'
+import CustomTextField from '@bit/totalsoft_oss.react-mui.custom-text-field'
+import DeleteButton from '@bit/totalsoft_oss.react-mui.delete-button'
+import { useTranslation } from 'react-i18next'
+
+const useStyles = makeStyles(tableStyles)
+
+const MyConferenceSpeakerData = props => {
+  const { speaker, dispatch, index } = props
+  const classes = useStyles()
+  const { t } = useTranslation()
+
+  return (
+    <Tr>
+      <Td className={classes.tableContent}>
+        <CustomTextField fullWidth />
+      </Td>
+      <Td className={classes.tableContent}>
+        <CustomTextField fullWidth />
+      </Td>
+      <Td className={classes.tableContent}>
+        <CustomTextField fullWidth isNumeric />
+      </Td>
+      <Td className={classes.tableContent}>
+        <Checkbox color='secondary' />
+      </Td>
+      <Td className={classes.tableContent}>
+        <DeleteButton title={t('General.Buttons.DeleteSpeaker')} />
+      </Td>
+    </Tr>
+  )
+}
+MyConferenceSpeakerData.propTypes = {
+  speaker: PropTypes.object.isRequired,
+  dispatch: PropTypes.func.isRequired,
+  index: PropTypes.number.isRequired
+}
+
+export default MyConferenceSpeakerData
--- src/features/myConference/edit/components/MyConferenceSpeakers.js
+++ src/features/myConference/edit/components/MyConferenceSpeakers.js
@@ -1,7 +1,49 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import MyConferenceInfo from './MyConferenceInfo'
+import { Grid, makeStyles } from '@material-ui/core'
+import { Table, Thead, Tbody, Tr, Th } from 'react-super-responsive-table'
+import { useTranslation } from 'react-i18next'
+import tableStyles from 'assets/jss/components/tableStyle'
+import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css'
+import MyConferenceSpeakerData from './MyConferenceSpeakerData'
+const useStyles = makeStyles(tableStyles)
 
-const MyConferenceSpeakers = () => {
-  return 'ffff'
+const MyConferenceSpeakers = props => {
+  const { speakers } = props
+  const { t } = useTranslation()
+  const classes = useStyles()
+
+  return (
+    <>
+      <Grid className={classes.enableScrollX}>
+        <Table classname={classes.Table}>
+          <Thead>
+            <Tr>
+              <Th className={classes.tableHeader}>{t('Speaker.Name')}</Th>
+              <Th className={classes.tableHeader}>{t('Speaker.Nationality')}</Th>
+              <Th className={classes.tableHeader}>{t('Speaker.Rating')}</Th>
+              <Th className={classes.tableHeader}>{t('Speaker.MainSpeaker')}</Th>
+              <Th className={classes.tableHeader}></Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {speakers?.map((speaker, index) => (
+              <MyConferenceSpeakerData key={speaker?.id} speaker={speaker} index={index} />
+            ))}
+          </Tbody>
+        </Table>
+      </Grid>
+    </>
+  )
+}
+
+MyConferenceSpeakers.propTypes = {
+  speakers: PropTypes.array
+}
+
+MyConferenceSpeakers.defaultProps = {
+  speakers: [{}]
 }
 
 export default MyConferenceSpeakers
