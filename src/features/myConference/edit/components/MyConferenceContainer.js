5d1734d2bae76266ad2d0d49287a53494f90f724
Andrei Marian Patru <andreiuttzzumarian00@gmail.com>
2021-09-24 15:37:58 +0300

    conference item component

--- src/features/myConference/edit/components/MyConferenceContainer.js
+++ src/features/myConference/edit/components/MyConferenceContainer.js
@@ -1,7 +1,40 @@
-import React from 'react'
+import SaveButton from '@bit/totalsoft_oss.react-mui.save-button'
+import MyConferenceHeader from 'features/myConference/list/components/MyConferenceHeader'
+import { useHeader } from 'providers/AreasProvider'
+import React, { useEffect } from 'react'
+import { useTranslation } from 'react-i18next'
+import { types, categories, countries, counties, cities } from 'utils/mocks/addConference'
+import MyConference from './MyConference'
 
 const MyConferenceContainer = () => {
-  return 'ddddd'
+  const { t } = useTranslation()
+  const [, setHeader] = useHeader()
+  useEffect(() => () => setHeader(null, []))
+
+  useEffect(() => {
+    setHeader(<MyConferenceHeader actions={<SaveButton title={t('General.Buttons.Save')} />} />)
+  }, [setHeader, t])
+
+  const { loading, data } = {
+    loading: false,
+    data: {
+      typeList: types,
+      categoryList: categories,
+      countryList: countries,
+      countyList: counties,
+      cityList: cities
+    }
+  }
+
+  return (
+    <MyConference
+      types={data?.typeList}
+      categories={data?.categoryList}
+      countries={data?.countryList}
+      counties={data?.countyList}
+      cities={data?.cityList}
+    />
+  )
 }
 
 export default MyConferenceContainer
---
+++ src/features/myConference/edit/components/MyConferenceLocation.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const MyConferenceLocation = () => {
+  return 'ffff'
+}
+
+export default MyConferenceLocation
