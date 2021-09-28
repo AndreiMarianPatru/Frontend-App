901512dfcb81f9fe2d1944942ab682aca1865020
Andrei Marian Patru <andreiuttzzumarian00@gmail.com>
2021-09-28 16:14:37 +0300

    hello world query

--- src/features/helloWorld/HelloWorld.js
+++ src/features/helloWorld/HelloWorld.js
@@ -1,5 +1,15 @@
+import { useQuery } from '@apollo/client'
+import { LoadingFakeText } from '@bit/totalsoft_oss.react-mui.kit.core'
+import { MY_FIRST_QUERY } from './queries/MyFirstQuery'
+import React from 'react'
+
 function HelloWorld() {
-  return 'Hello World!👋'
+  const { loading, data } = useQuery(MY_FIRST_QUERY)
+  if (loading) {
+    return <LoadingFakeText lines={10} />
+  }
+
+  return data?.myFirstEndpoint
 }
 
 export default HelloWorld
