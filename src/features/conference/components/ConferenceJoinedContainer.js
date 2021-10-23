7472501fb5660792a4a72c1419331b7b3a0a370a
Andrei Marian Patru <andreiuttzzumarian00@gmail.com>
2021-10-23 19:16:47 +0300

    youtube video added

--- src/features/conference/components/ConferenceJoinedContainer.js
+++ src/features/conference/components/ConferenceJoinedContainer.js
@@ -1,10 +1,15 @@
 import { Typography } from '@material-ui/core'
 import React from 'react'
+import ReactPlayer from 'react-player'
+import YouTubePlayer from 'react-player/lib/players/YouTube'
 
 const ConferenceJoinedContainer = () => {
   return (
     <>
-      <Typography>New Conference</Typography>
+      <div className='App'>
+        <Typography>Getting Started With React.js: Welcome</Typography>
+        <ReactPlayer url='https://www.youtube.com/watch?v=FrCgZZXXK9I&ab_channel=EnvatoTuts%2B' controls={true} />
+      </div>
     </>
   )
 }
